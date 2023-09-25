75c24c0c7b8f528de8c9fd85619d0c6159700453
Andrey <oinkpage@gmail.com>
2023-09-25 19:23:27 +0300

    create full description for database entity class JwtStorageEntity

--- backend/src/entities/jwt_storage.entity.ts
+++ backend/src/entities/jwt_storage.entity.ts
@@ -1,6 +1,31 @@
-import { Entity } from "typeorm";
+import { Column, Entity, JoinColumn, OneToOne, PrimaryColumn } from "typeorm";
+import UserEntity from "./user.entity";
 
 @Entity('jwt_storage')
 export default class JwtStorageEntity{
+  @PrimaryColumn({
+    name: 'id',
+    type: "char",
+    length: 256,
+    nullable: false
+  })
+  id: string
 
+  @Column({
+    name: 'userId',
+    type: "char",
+    length: 256,
+    nullable: false
+  })
+  @OneToOne(() => UserEntity)
+  @JoinColumn()
+  user: UserEntity
+
+  @Column({
+    name: 'refreshToken',
+    type: 'char',
+    length: 1024,
+    nullable: false
+  })
+  refreshToken: string
 }
