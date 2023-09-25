f95b2ead74bddc87f4cf3e775d3004c20227c146
Andrey <oinkpage@gmail.com>
2023-09-25 13:38:44 +0300

    create full description for entity class BonusCardEntity

--- backend/src/entities/bonus_card.entity.ts
+++ backend/src/entities/bonus_card.entity.ts
@@ -1,6 +1,42 @@
-import { Entity } from "typeorm";
+import { Column, Entity, JoinColumn, OneToOne, PrimaryColumn } from "typeorm";
+import UserEntity from "./user.entity";
+import BonusLevelEntity from "./bonus_level.entity";
 
 @Entity('bonus_card')
 export default class BonusCardEntity{
+  @PrimaryColumn({
+    name: 'id',
+    type: 'char',
+    length: 256,
+    nullable: false
+  })
+  id: string
 
+  @Column({
+    name: 'userId',
+    type: "integer",
+    nullable: false,
+    unique: true
+  })
+  @OneToOne(user => UserEntity)
+  @JoinColumn()
+  user: UserEntity
+
+  @Column({
+    name: 'levelId',
+    type: "integer",
+    nullable: false,
+    unique: true
+  })
+  @OneToOne(user => BonusLevelEntity)
+  @JoinColumn()
+  level: BonusLevelEntity
+
+  @Column({
+    name: 'code',
+    type: 'char',
+    length: 256,
+    nullable: false
+  })
+  code: string
 }
