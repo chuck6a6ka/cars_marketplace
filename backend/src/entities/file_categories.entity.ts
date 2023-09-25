ad87f25a3dd6bc6a140c7a5f075a463ccf761482
Andrey <oinkpage@gmail.com>
2023-09-25 13:51:45 +0300

    create full description for database entity FileCategoriesEntity

--- backend/src/entities/file_categories.entity.ts
+++ backend/src/entities/file_categories.entity.ts
@@ -1,6 +1,26 @@
-import { Entity } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('file_categories')
 export default class FileCategoriesEntity{
+  @PrimaryGeneratedColumn({
+    name: 'id',
+    type: "integer"
+  })
+  id: number
 
+  @Column({
+    name: 'title',
+    type: "char",
+    length: 64,
+    nullable: false
+  })
+  title: string
+
+  @Column({
+    name: 'code',
+    type: "char",
+    length: 64,
+    nullable: false
+  })
+  code: string
 }
