0d3b1f9e56a9b31c6dec9d9dbc4e26a463eb55b9
Andrey <oinkpage@gmail.com>
2023-09-25 13:43:49 +0300

    create full description for entity class TypeEntity

--- backend/src/entities/type.entity.ts
+++ backend/src/entities/type.entity.ts
@@ -1,6 +1,34 @@
-import { Entity } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('type')
 export default class TypeEntity{
+  @PrimaryGeneratedColumn({
+    name: 'id',
+    type: "integer"
+  })
+  id: number
 
+  @Column({
+    name: 'title',
+    type: "char",
+    length: 256,
+    nullable: false
+  })
+  title: string
+
+  @Column({
+    name: 'code',
+    type: "char",
+    length: 256,
+    nullable: false
+  })
+  code: string
+
+  @Column({
+    name: 'isAlive',
+    type: "boolean",
+    nullable: false,
+    default: true,
+  })
+  isAlive: boolean
 }
