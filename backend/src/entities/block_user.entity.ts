99fd722ed8d1ed334d22fdc0f6d007f253fa739b
Andrey <oinkpage@gmail.com>
2023-09-25 13:33:04 +0300

    create full description for entity class UserEntity

--- backend/src/entities/block_user.entity.ts
+++ backend/src/entities/block_user.entity.ts
@@ -1,6 +1,65 @@
-import { Entity } from "typeorm";
+import { Column, Entity, JoinColumn, OneToOne, PrimaryColumn } from "typeorm";
+import ReasonEntity from "./reason.entity";
+import UserEntity from "./user.entity";
 
 @Entity('block_user')
 export default class BlockUserEntity{
+ @PrimaryColumn({
+   name: 'id',
+   type: 'char',
+   length: 256,
+   nullable: false,
+ })
+  id: string
 
+  @Column({
+    name: 'userId',
+    type: "integer",
+    nullable: false
+  })
+  @OneToOne(user => UserEntity)
+  @JoinColumn()
+  user: UserEntity
+
+  @Column({
+    name: "reasonId",
+    type: "integer",
+    nullable: false,
+  })
+  @OneToOne(reason => ReasonEntity)
+  @JoinColumn()
+  reason: ReasonEntity
+
+  @Column({
+    name: 'comment',
+    type: "char",
+    length: 256,
+    nullable: false
+  })
+  comment: string
+
+  @Column({
+    name: 'timestamp',
+    type: "char",
+    length: 64,
+    nullable: false,
+    default: new Date().getTime()
+  })
+  timestamp: string
+
+  @Column({
+    name: 'endBlockTimestamp',
+    type: "char",
+    length: 64,
+    nullable: false
+  })
+  endBlockTimestamp: string
+
+  @Column({
+    name: 'isActive',
+    type: "boolean",
+    nullable: false,
+    default: true
+  })
+  isActive: boolean
 }
