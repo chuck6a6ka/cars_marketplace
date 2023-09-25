b7439b063f90fc7296da93849541a8f3a1c7bf9c
Andrey <oinkpage@gmail.com>
2023-09-25 19:42:21 +0300

    remove dependencies

--- backend/src/entities/goods_properties.entity.ts
+++ backend/src/entities/goods_properties.entity.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryColumn } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
 import GoodsEntity from "./goods.entity";
 import PropertiesEntity from "./properties.entity";
 
