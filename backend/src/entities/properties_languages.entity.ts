024cf22e2fabf7316099856520fc95082040daa6
Andrey <oinkpage@gmail.com>
2023-09-25 19:40:00 +0300

    create full description for database entity class PropertiesLanguagesEntity

--- backend/src/entities/properties_languages.entity.ts
+++ backend/src/entities/properties_languages.entity.ts
@@ -1,6 +1,44 @@
-import { Entity } from "typeorm";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import PropertiesEntity from "./properties.entity";
+import LanguagesEntity from "./languages.entity";
 
 @Entity('properties_languages')
 export default class PropertiesLanguagesEntity{
+ @PrimaryGeneratedColumn({
+   name: 'id',
+   type: "integer"
+ })
+  id: number
 
+  @Column({
+    name: 'propertyId',
+    type: "integer",
+    nullable: false
+  })
+  @ManyToOne(()=> PropertiesEntity, (record) => record.translations)
+  property: PropertiesEntity
+
+  @Column({
+    name: 'languageId',
+    type: "integer",
+    nullable: false
+  })
+  @ManyToOne(()=> LanguagesEntity, (record) => record.properties)
+  language: LanguagesEntity
+
+  @Column({
+    name: 'title',
+    type: 'char',
+    length: 256,
+    nullable: false
+  })
+  title: string
+
+  @Column({
+    name: 'data',
+    type: 'char',
+    length: 256,
+    nullable: false
+  })
+  data: string
 }
