b4a0dd0ea633fa98038de64750fb740982e67fd8
Andrey <oinkpage@gmail.com>
2023-09-25 19:06:25 +0300

    create full description for database entity class PhotosGoodsEntity

--- backend/src/entities/photos_goods.entity.ts
+++ backend/src/entities/photos_goods.entity.ts
@@ -1,6 +1,40 @@
-import { Entity } from "typeorm";
+import { Column, Entity, JoinColumn, OneToOne, PrimaryColumn } from "typeorm";
+import GoodsEntity from "./goods.entity";
+import FilesEntity from "./files.entity";
 
 @Entity('photos_goods')
 export default class PhotosGoodsEntity{
+  @PrimaryColumn({
+    name: 'id',
+    type: "char",
+    length: 256,
+    nullable: false
+  })
+  id: number
 
+  @Column({
+    name: 'goodsId',
+    type: "integer",
+    nullable: false
+  })
+  @OneToOne(() => GoodsEntity)
+  @JoinColumn()
+  goods: GoodsEntity
+
+  @Column({
+    name: 'fileId',
+    type: "integer",
+    nullable: false
+  })
+  @OneToOne(() => FilesEntity)
+  @JoinColumn()
+  file: FilesEntity
+
+  @Column({
+    name: 'isAlive',
+    type: "boolean",
+    nullable: false,
+    default: true,
+  })
+  isAlive: boolean
 }
