8efe059532382c006c784b228f6e0c7810785183
Andrey <oinkpage@gmail.com>
2023-09-25 19:39:48 +0300

    create full description for database entity class LanguagesEntity

--- backend/src/entities/languages.entity.ts
+++ backend/src/entities/languages.entity.ts
@@ -1,6 +1,34 @@
-import { Entity } from "typeorm";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import PropertiesLanguagesEntity from "./properties_languages.entity";
+import ReviewsEntity from "./reviews.entity";
 
 @Entity('languages')
 export default class LanguagesEntity{
+@PrimaryGeneratedColumn({
+  name: 'id',
+  type: "integer",
+})
+  id: number
 
+  @Column({
+    name: 'title',
+    type: 'char',
+    length: 64,
+    nullable: false
+  })
+  title: string
+
+  @Column({
+    name: 'code',
+    type: 'char',
+    length: 64,
+    nullable: false
+  })
+  code: string
+
+  @OneToMany(()=> PropertiesLanguagesEntity, (record) => record.language)
+  properties: PropertiesLanguagesEntity[]
+
+  @OneToMany(()=> ReviewsEntity, (record) => record)
+  reviews: ReviewsEntity[]
 }
