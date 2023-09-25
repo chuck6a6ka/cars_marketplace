248beb39571d0e3ec4a26d1240bda1f837b6456e
Andrey <oinkpage@gmail.com>
2023-09-25 19:50:16 +0300

    create full description for database entity class ShopGoodsEntity

--- backend/src/entities/shop_goods.entity.ts
+++ backend/src/entities/shop_goods.entity.ts
@@ -1,6 +1,47 @@
-import { Entity } from "typeorm";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToOne,
+  PrimaryColumn,
+} from 'typeorm';
+import GoodsEntity from './goods.entity';
+import ShopsEntity from './shops.entity';
 
 @Entity('shop_goods')
-export default class ShopGoodsEntity{
+export default class ShopGoodsEntity {
+  @PrimaryColumn({
+    name: 'id',
+    type: 'char',
+    length: 256,
+    nullable: false,
+  })
+  id: string;
 
+  @Column({
+    name: 'goodsId',
+    type: 'char',
+    length: 256,
+    nullable: false,
+  })
+  @OneToOne(() => GoodsEntity)
+  @JoinColumn()
+  goods: GoodsEntity;
+
+  @Column({
+    name: 'shopId',
+    type: 'integer',
+    nullable: false,
+  })
+  @ManyToOne(() => ShopsEntity, (shop) => shop)
+  shop: ShopsEntity;
+
+  @Column({
+    name: 'isAlive',
+    type: 'boolean',
+    nullable: false,
+    default: true,
+  })
+  isAlive: boolean;
 }
