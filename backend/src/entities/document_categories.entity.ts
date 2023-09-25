465269f2e5a1f9b3f5c69130ecf5933e71d287ee
Andrey <oinkpage@gmail.com>
2023-09-25 13:48:20 +0300

    create full description for entity class DocumentCategoriesEntity

--- backend/src/entities/document_categories.entity.ts
+++ backend/src/entities/document_categories.entity.ts
@@ -1,6 +1,34 @@
-import { Entity } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity('document_categories')
 export default class DocumentCategoriesEntity{
+@PrimaryGeneratedColumn({
+  name: 'id',
+  type: "integer"
+})
+  id: number
 
+  @Column({
+    name: 'title',
+    type: "char",
+    length: 64,
+    nullable: false
+  })
+  title: string
+
+  @Column({
+    name: 'code',
+    type: "char",
+    length: 64,
+    nullable: false
+  })
+  code: string
+
+  @Column({
+    name: 'isAlive',
+    type: "boolean",
+    nullable: false,
+    default: true,
+  })
+  isAlive: boolean
 }
