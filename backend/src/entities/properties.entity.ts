2f405752075c4e22048a61b733841844d09291c8
Andrey <oinkpage@gmail.com>
2023-09-25 19:39:53 +0300

    create full description for database entity class PropertiesEntity

--- backend/src/entities/properties.entity.ts
+++ backend/src/entities/properties.entity.ts
@@ -1,6 +1,42 @@
-import { Entity } from "typeorm";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import GoodsPropertiesEntity from "./goods_properties.entity";
+import PropertiesLanguagesEntity from "./properties_languages.entity";
 
 @Entity('properties')
 export default class PropertiesEntity{
+  @PrimaryGeneratedColumn({
+    name: 'id',
+    type: "integer"
+  })
+  id: number
 
+  @Column({
+    name: 'title',
+    type: "char",
+    length: 256,
+    nullable: false
+  })
+  title: string
+
+  @Column({
+    name: 'code',
+    type: "char",
+    length: 256,
+    nullable: false
+  })
+  code: string
+
+  @Column({
+    name: 'isAlive',
+    type: "boolean",
+    nullable: false,
+    default: true,
+  })
+  isAlive: boolean
+
+  @OneToMany(() => GoodsPropertiesEntity, (record) => record.property)
+  goodsProperties: GoodsPropertiesEntity[]
+
+  @OneToMany(()=> PropertiesLanguagesEntity, (record) => record.property)
+  translations: PropertiesLanguagesEntity[]
 }
