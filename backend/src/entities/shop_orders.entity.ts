bc68e1768c6d9d2ae928b696feb91e23ce868fb6
Andrey <oinkpage@gmail.com>
2023-09-25 19:51:42 +0300

    create full description for database entity class ShopOrdersEntity

--- backend/src/entities/shop_orders.entity.ts
+++ backend/src/entities/shop_orders.entity.ts
@@ -1,6 +1,32 @@
-import { Entity } from "typeorm";
+import { Column, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryColumn } from "typeorm";
+import ShopsEntity from "./shops.entity";
+import OrdersEntity from "./orders.entity";
 
 @Entity('shop_orders')
 export default class ShopOrdersEntity{
+  @PrimaryColumn({
+    name: 'id',
+    type: 'char',
+    length: 256,
+    nullable: false,
+  })
+  id: string;
 
+  @Column({
+    name: 'orderId',
+    type: 'char',
+    length: 256,
+    nullable: false,
+  })
+  @OneToOne(() => OrdersEntity)
+  @JoinColumn()
+  order: OrdersEntity;
+
+  @Column({
+    name: 'shopId',
+    type: 'integer',
+    nullable: false,
+  })
+  @ManyToOne(() => ShopsEntity, (shop) => shop)
+  shop: ShopsEntity;
 }
